8805cecf2dd6b12a099dfdbd114eaf170f006dd5
Rahul <rahul@Rahul.Rahul>
2021-03-28 14:19:28 +0530

    Added Navbar & Footer

--- frontend/hospital/src/components/Footer.js
+++ frontend/hospital/src/components/Footer.js
@@ -2,9 +2,163 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="f-items default-padding">
+            <div class="col-md-4 item">
+              <div id="custom_html-2">
+                <div class="textwidget custom-html-widget">
+                  <div class="f-item">
+                    <h4>About</h4>
+                    Excellence decisively nay man yet impression for contrasted
+                    remarkably. There spoke happy for you are out. Fertile how
+                    old address.
+                    <h2>
+                      <i class="fas fa-phone"></i> +123 456 7890
+                    </h2>
+                    <div class="opening-info">
+                      <h5>Opening Hours</h5>
+                      <ul>
+                        <li>
+                          Mon - Tues :
+                          <div class="pull-right">6.00 am - 10.00 pm</div>
+                        </li>
+                        <li>
+                          Wednes - Thurs :
+                          <div class="pull-right">8.00 am - 6.00 pm</div>
+                        </li>
+                        <li>
+                          Sun :<div class="pull-right closed">Closed</div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 item">
+              <div id="custom_html-3">
+                <div class="textwidget custom-html-widget">
+                  <div class="f-item link">
+                    <h4>Our Depeartment</h4>
+                    <ul>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-arrow-right"></i> Medecine and Health
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-arrow-right"></i> Dental Care and
+                          Surgery
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-arrow-right"></i> Eye Treatment
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-arrow-right"></i> Children Chare
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-arrow-right"></i> Nuclear magnetic
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-arrow-right"></i> Traumatology
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-arrow-right"></i> X-ray
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 item">
+              <div id="custom_html-4">
+                <div class="textwidget custom-html-widget">
+                  <div class="f-item twitter-widget">
+                    <h4>Latest tweets</h4>
+                    <div class="twitter-item">
+                      <div class="twitter-content">
+                        <a href="#">@Becare</a>
+                        Looking for an awesome CREATIVE WordPress Theme? Find it
+                        here:
+                        <a
+                          href="http://t.co/0WWEMQEQ48"
+                          target="_blank"
+                          rel="noopener"
+                        >
+                          http://t.co/0WWEMQEQ48
+                        </a>
+                      </div>
+                      <div class="twitter-context">
+                        <i class="fab fa-twitter"></i>
+                        <span class="twitter-date"> 01 day ago</span>
+                      </div>
+                    </div>
+                    <div class="twitter-item">
+                      <div class="twitter-content">
+                        <a href="#">@Jisham</a>
+                        It is a long established fact that a reader will be
+                        distracted by the readable . Find it here:
+                        <a
+                          href="http://t.co/0WWEMQEQ48"
+                          target="_blank"
+                          rel="noopener"
+                        >
+                          http://t.co/0WWEMQEQ48
+                        </a>
+                      </div>
+                      <div class="twitter-context">
+                        <i class="fab fa-twitter"></i>
+                        <span class="twitter-date"> 02 days ago</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer-bottom bg-dark text-light">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <p>
+                &copy; Copyright 2019. All Rights Reserved by{" "}
+                <a href="#">Sttheme</a>
+              </p>
+            </div>
+            <div class="col-md-6 text-right link">
+              <ul>
+                <li>
+                  <a href="#">Terms of user</a>
+                </li>
+                <li>
+                  <a href="#">License</a>
+                </li>
+                <li>
+                  <a href="#">Support</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
--- frontend/hospital/src/components/Navbar.js
+++ frontend/hospital/src/components/Navbar.js
@@ -1,11 +1,274 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { logout } from "../actions/auth";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Alert from "./Alert";
 
-const Navbar = () => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authlinks = (
+    <li
+      id="menu-item-408"
+      class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-408"
+    >
+      <a title="Contact" href="#" onClick={logout}>
+        Logout
+      </a>
+    </li>
+  );
+
+  const guestlinks = (
+    <Fragment>
+      <li
+        id="menu-item-408"
+        class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-408"
+      >
+        <Link title="Contact" to="/login">
+          LogIn
+        </Link>
+      </li>
+      <li
+        id="menu-item-408"
+        class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-408"
+      >
+        <Link title="Contact" to="/signup">
+          SignUp
+        </Link>
+      </li>
+    </Fragment>
+  );
+
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <body class="home page-template page-template-page-templates page-template-home page-template-page-templateshome-php page page-id-44 wpb-js-composer js-comp-ver-5.4.7 vc_responsive">
+      <div class="se-pre-con"></div>
+      <div class="top-bar-area inline inc-border">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8 address-info text-left">
+              <div class="info box">
+                <ul>
+                  <li>
+                    <i class="fas fa-map-marker-alt"></i>
+                    California, TX 70240
+                  </li>
+                  <li>
+                    <i class="fas fa-envelope-open"></i>
+                    Info@gmail.com
+                  </li>
+                  <li>
+                    <i class="fas fa-phone"></i>
+                    Call Us : +02151 7778 009
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-4 bar-btn text-right">
+              <a href="#">MAKE APPOINMENT</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <header id="home">
+        <nav class="navbar navbar-default attr-border navbar-sticky bootsnav">
+          <div class="container">
+            <div class="row">
+              <div class="top-search">
+                <div class="input-group">
+                  <form action="https://sttheme.com/demosd/medihub/">
+                    <input
+                      type="text"
+                      name="s"
+                      id="s"
+                      class="form-control"
+                      placeholder="Search"
+                    />
+                    <button type="submit">
+                      <i class="fas fa-search"></i>
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="container">
+            <div class="attr-nav">
+              <ul>
+                <li class="search">
+                  <a href="#">
+                    <i class="fa fa-search"></i>
+                  </a>
+                </li>
+                <li class="side-menu">
+                  <a href="#">
+                    <i class="fa fa-bars"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="navbar-header">
+              <button
+                type="button"
+                class="navbar-toggle"
+                data-toggle="collapse"
+                data-target="#navbar-menu"
+              >
+                <i class="fa fa-bars"></i>
+              </button>
+              <a
+                class="navbar-brand"
+                href="https://sttheme.com/demosd/medihub/"
+              >
+                <img
+                  src="http://sttheme.com/demosd/medihub/wp-content/uploads/2019/06/logo.png"
+                  class="logo"
+                  alt="Logo"
+                />
+              </a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbar-menu">
+              <ul class="nav navbar-nav navbar-right" data-in="#" data-out="#">
+                <li
+                  id="menu-item-369"
+                  class="menu-item menu-item-type-custom menu-item-object-custom current-menu-ancestor current-menu-parent menu-item-has-children nav-item menu-item-369 dropdown  active"
+                >
+                  <a
+                    title="Home"
+                    href="#"
+                    class="dropdown-toggle smooth-menu"
+                    data-toggle="dropdown"
+                  >
+                    Home<span class="hidden-lg span-drop"></span>
+                  </a>
+                </li>
+                <li
+                  id="menu-item-371"
+                  class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children nav-item menu-item-371 dropdown "
+                >
+                  <a
+                    title="Pages"
+                    href="#"
+                    class="dropdown-toggle smooth-menu"
+                    data-toggle="dropdown"
+                  >
+                    Pages<span class="hidden-lg span-drop"></span>
+                  </a>
+                </li>
+                <li
+                  id="menu-item-382"
+                  class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children nav-item menu-item-382 dropdown "
+                >
+                  <a
+                    title="Departments"
+                    href="#"
+                    class="dropdown-toggle smooth-menu"
+                    data-toggle="dropdown"
+                  >
+                    Departments<span class="hidden-lg span-drop"></span>
+                  </a>
+                  <ul role="menu" class="dropdown-menu animated #">
+                    <li
+                      id="menu-item-383"
+                      class="menu-item menu-item-type-custom menu-item-object-custom nav-item menu-item-383"
+                    >
+                      <a
+                        title="Medecine And Health"
+                        href="http://sttheme.com/demosd/medihub/?services=medecine-and-health"
+                      >
+                        Medecine And Health
+                      </a>
+                    </li>
+                    <li
+                      id="menu-item-384"
+                      class="menu-item menu-item-type-custom menu-item-object-custom nav-item menu-item-384"
+                    >
+                      <a
+                        title="Dental Care and Surgery"
+                        href="http://sttheme.com/demosd/medihub/?services=dental-care-and-surgery"
+                      >
+                        Dental Care and Surgery
+                      </a>
+                    </li>
+                    <li
+                      id="menu-item-385"
+                      class="menu-item menu-item-type-custom menu-item-object-custom nav-item menu-item-385"
+                    >
+                      <a
+                        title="Eye Treatment"
+                        href="http://sttheme.com/demosd/medihub/?services=eye-treatment"
+                      >
+                        Eye Treatment
+                      </a>
+                    </li>
+                    <li
+                      id="menu-item-386"
+                      class="menu-item menu-item-type-custom menu-item-object-custom nav-item menu-item-386"
+                    >
+                      <a
+                        title="Children Chare"
+                        href="http://sttheme.com/demosd/medihub/?services=children-chare"
+                      >
+                        Children Chare
+                      </a>
+                    </li>
+                    <li
+                      id="menu-item-387"
+                      class="menu-item menu-item-type-custom menu-item-object-custom nav-item menu-item-387"
+                    >
+                      <a
+                        title="Traumatology"
+                        href="http://sttheme.com/demosd/medihub/?services=nuclear-magnetic"
+                      >
+                        Traumatology
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li
+                  id="menu-item-388"
+                  class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children nav-item menu-item-388 dropdown "
+                >
+                  <a
+                    title="Doctors"
+                    href="#"
+                    class="dropdown-toggle smooth-menu"
+                    data-toggle="dropdown"
+                  >
+                    Doctors
+                    <span class="hidden-lg span-drop"></span>
+                  </a>
+                </li>
+                <li
+                  id="menu-item-408"
+                  class="menu-item menu-item-type-post_type menu-item-object-page nav-item menu-item-408"
+                >
+                  <a
+                    title="Contact"
+                    href="https://sttheme.com/demosd/medihub/?page_id=282"
+                  >
+                    Contact
+                  </a>
+                </li>
+                {!loading && (
+                  <Fragment>
+                    {isAuthenticated ? authlinks : guestlinks}
+                  </Fragment>
+                )}
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </body>
   );
 };
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
 
-export default Navbar;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+export default connect(mapStateToProps, { logout })(Navbar);
